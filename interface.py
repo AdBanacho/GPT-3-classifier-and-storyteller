d010a6d6b60814bae1cb9da883728661c8879f79
AdBanacho <48532170+AdBanacho@users.noreply.github.com>
2022-04-25 01:56:15 +0200

    Add files via upload

--- interface.py
+++ interface.py
@@ -18,17 +18,13 @@ PAGES = {
 name = st.text_input("Please, write your name")
 api_key = st.text_input("OpenAI API Key:", type="password")
 
-
-
 col1, col2 = st.columns([4, 1])
 
-if api_key:
+if api_key and name:
     GPT = Gpt(api_key)
+    title = GPT.gpt_3(name, c.NAME_TEMP, p.generate_prompt_greetings)
+    col1.header(title)
     selection = col2.radio('Go to:', list(PAGES.keys()))
     page = PAGES[selection]
     with st.spinner("Processing..."):
         page.Interface(GPT).run()
-
-if name:
-    title = GPT.gpt_3(name, c.NAME_TEMP, p.generate_prompt_greetings)
-    col1.header(title)
